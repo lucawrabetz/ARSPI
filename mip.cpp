371dcdb092941557326923ad2b00d77a22eef33a
lucawrabetz <luw28@pitt.edu>
2024-01-26 17:21:30 -0500

    Remove GREEDY from SOLVERS in mip.cpp.

--- mip.cpp
+++ mip.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
   const int max_k = std::stoi(argv[3]);
   const int min_budget = std::stoi(argv[4]);
   const int max_budget = std::stoi(argv[5]);
-  const std::vector<ASPI_Solver> solvers{MIP, GREEDY};
+  const std::vector<ASPI_Solver> solvers{MIP};
   RunAllInstancesInSetDirectory(min_k, max_k, min_budget, max_budget, set_name,
                                 solvers, greedy_mip_gap_threshold);
 }
